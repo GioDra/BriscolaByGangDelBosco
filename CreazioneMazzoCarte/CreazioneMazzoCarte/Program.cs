e0860da6226691eab977c117f07cd76c1ad150e1
giovanni.drago@edu.itspiemonte.it <giovanni.drago@edu.itspiemonte.it>
2018-05-31 09:50:15 +0200

    Prova (dinuovo)

--- CreazioneMazzoCarte/CreazioneMazzoCarte/Program.cs
+++ CreazioneMazzoCarte/CreazioneMazzoCarte/Program.cs
@@ -19,69 +19,6 @@ namespace CreazioneMazzoCarte
             Console.ReadLine();
         }
 
-        static void RaccoltaCarte(List<Carta> CardsPack)
-        {
-            enumSeed.seed temp_seed = enumSeed.seed.B;
-            String name = "";
-
-            //Cicli per la creazione di n carte per differenti semi
-            for (int i = 0; i < 4; i++)
-            {
-                switch (i)
-                {
-                    case 1:
-                        temp_seed = enumSeed.seed.O;
-                        break;
-                    case 2:
-                        temp_seed = enumSeed.seed.S;
-                        break;
-                    case 3:
-                        temp_seed = enumSeed.seed.C;
-                        break;
-
-                }
-                for (int k = 1; k <= 10; k++)
-                {
-                    switch (k)
-                    {
-                        case 1:
-                            name = "Asso";
-                            break;
-                        case 8:
-                            name = "Donna";
-                            break;
-                        case 9:
-                            name = "Fante";
-                            break;
-                        case 10:
-                            name = "Re";
-                            break;
-                        default:
-                            name = k.ToString();
-                            break;
-                    }
-                    CardsPack.Add(new Carta(k, name, temp_seed));
-                }
-            }
-            
-        }
-
-        static void Mescola(List<Carta> CardsPack)
-        {
-            Random rnd = new Random();
-            Carta c = new Carta();
-            int indiceA;
-            int indiceB;
-            for (int i = 0; i < 400; i++)
-            {
-                indiceA = rnd.Next(0, (CardsPack.Count() - 1));
-                c = CardsPack[indiceA];
-                indiceB = rnd.Next(0, (CardsPack.Count() - 1));
-                CardsPack[indiceA] = CardsPack[indiceB];
-                CardsPack[indiceB] = c;
-            }
-        }
-        
 
         static void Partita(Mazzo m)
         {
